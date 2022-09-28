c6622c1a2e955aeba0de25ae238545420b206981
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-27 21:01:43 -0300

    Término da parte 1 do exercício.

--- projeto.js
+++ projeto.js
@@ -46,14 +46,32 @@ class PassagemAerea{
     Assento
     PrimeiraClasse
     Valor
-    Passageiro
+    Passageiro 
     Voo
     
-    CalcularValor(){
+    constructor(assento,primeiraClasse,valor,passageiro,voo){
+        this.Assento = assento  
+        this.PrimeiraClasse = primeiraClasse  
+        this.Valor = this.CalcularValor(valor)
+        this.Passageiro = passageiro 
+        this.Voo = voo 
+    }
 
+    CalcularValor(valor){
+        if(this.PrimeiraClasse == true){
+            valor = valor * 1.5
+        }
+        return valor
     }
 
     ExibirResumo(){
-        
+        alert("Passagem no nome de " +this.Passageiro.Nome+ " no assento " +this.Assento+ " do voo " +this.Voo.Numero+ " com destino para " +this.Voo.LocalDestino+ ".")
     }
 }
+// let PrimeiroCliete = new Cliente("Luiz", "00000000001", "30/04/1998")
+let VooIda = new Voo("Gol",1,"15/02/2023","12:45","São Paulo","EUA")
+let VooVolta = new Voo("Gol",2,"05/03/2023","13:15","EUA","São Paulo")
+let PassagemIda = new PassagemAerea("3C", true, 3000, new Cliente("Luiz", "00000000001", "30/04/1998"), VooIda);
+let PassagemVolta = new PassagemAerea("3C", false, 3000, PassagemIda.Passageiro, VooVolta);
+
+let PrimeiroPacote = new PacoteViagem(PassagemIda.Passageiro,PassagemIda,PassagemVolta,(PassagemIda.Valor + PassagemVolta.Valor))
